a8624b8d7388caabc1c251afb35ea9b676c73754
GiulianoMaPe <giulianomape.1806.14@gmail.com>
2025-08-22 23:56:34 -0500

    Árboles Worldgen

--- src/main/java/net/giuliano/peruviansdelight/worldgen/ModBiomeModifiers.java
+++ src/main/java/net/giuliano/peruviansdelight/worldgen/ModBiomeModifiers.java
@@ -1,18 +1,33 @@
 package net.giuliano.peruviansdelight.worldgen;
 
 import net.giuliano.peruviansdelight.PeruviansDelight;
+import net.minecraft.core.HolderSet;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstrapContext;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BiomeTags;
+import net.minecraft.world.level.levelgen.GenerationStep;
 import net.neoforged.neoforge.common.world.BiomeModifier;
+import net.neoforged.neoforge.common.world.BiomeModifiers;
 import net.neoforged.neoforge.registries.NeoForgeRegistries;
 
 public class ModBiomeModifiers {
+    public static final ResourceKey<BiomeModifier> ADD_TREE_LIMONERO = registerKey("add_tree_limonero");
+    public static final ResourceKey<BiomeModifier> ADD_TREE_PALTO = registerKey("add_tree_palto");
+
     public static void bootstrap(BootstrapContext<BiomeModifier> context) {
         var placedFeatures = context.lookup(Registries.PLACED_FEATURE);
         var biomes = context.lookup(Registries.BIOME);
 
+        context.register(ADD_TREE_LIMONERO, new BiomeModifiers.AddFeaturesBiomeModifier(
+                biomes.getOrThrow(BiomeTags.IS_OVERWORLD),
+                HolderSet.direct(placedFeatures.getOrThrow(ModPlacedFeatures.LIMONERO_PLACED_KEY)),
+                GenerationStep.Decoration.VEGETAL_DECORATION));
+        context.register(ADD_TREE_PALTO, new BiomeModifiers.AddFeaturesBiomeModifier(
+                biomes.getOrThrow(BiomeTags.IS_OVERWORLD),
+                HolderSet.direct(placedFeatures.getOrThrow(ModPlacedFeatures.PALTO_PLACED_KEY)),
+                GenerationStep.Decoration.VEGETAL_DECORATION));
     }
 
     private static ResourceKey<BiomeModifier> registerKey(String name) {
--- src/main/java/net/giuliano/peruviansdelight/worldgen/ModPlacedFeatures.java
+++ src/main/java/net/giuliano/peruviansdelight/worldgen/ModPlacedFeatures.java
@@ -1,9 +1,12 @@
 package net.giuliano.peruviansdelight.worldgen;
 
 import net.giuliano.peruviansdelight.PeruviansDelight;
+import net.giuliano.peruviansdelight.block.ModBlocks;
 import net.minecraft.core.Holder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.data.worldgen.placement.PlacementUtils;
+import net.minecraft.data.worldgen.placement.VegetationPlacements;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
@@ -13,9 +16,18 @@ import net.minecraft.world.level.levelgen.placement.PlacementModifier;
 import java.util.List;
 
 public class ModPlacedFeatures {
+    public static final ResourceKey<PlacedFeature> LIMONERO_PLACED_KEY = registerKey("limonero_placed");
+    public static final ResourceKey<PlacedFeature> PALTO_PLACED_KEY = registerKey("palto_placed");
+
     public static void bootstrap(BootstrapContext<PlacedFeature> context) {
         var configuredFeatures = context.lookup(Registries.CONFIGURED_FEATURE);
 
+        register(context, LIMONERO_PLACED_KEY, configuredFeatures.getOrThrow(ModConfiguredFeatures.LIMONERO_KEY),
+                VegetationPlacements.treePlacement(PlacementUtils.countExtra(0, 0.02f, 2),
+                        ModBlocks.SEMILLAS_LIMON.get()));
+        register(context, PALTO_PLACED_KEY, configuredFeatures.getOrThrow(ModConfiguredFeatures.PALTO_KEY),
+                VegetationPlacements.treePlacement(PlacementUtils.countExtra(0, 0.02f, 2),
+                        ModBlocks.SEMILLA_PALTA.get()));
     }
 
     private static ResourceKey<PlacedFeature> registerKey(String name) {
